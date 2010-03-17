af79234ea420cc5333894fd1bc0f54e8f323c6e4
xlrj <jrichard.lai@gmail.com>
2010-03-17 16:09:19 +0100

    Refactored leap year

--- piscine/leap_year.rb
+++ piscine/leap_year.rb
@@ -1,19 +1,7 @@
-
-boucle = 0
 puts "Give me a starting Year"
-startingYear = gets.chomp
+startingYear = gets.chomp.to_i
 puts "Give me a ending year"
-endingYear = gets.chomp
-boucle = startingYear.to_i
-while boucle <= endingYear.to_i
-  if boucle % 4 == 0
-    if boucle % 100 == 0 and boucle % 400 == 0
-      puts boucle.to_s + " is Leap year"
-    elsif(boucle % 100 == 0 and boucle % 400 > 0)
-      
-    else
-      puts boucle.to_s + " is Leap year"
-    end
-  end
-  boucle = boucle + 1
+endingYear = gets.chomp.to_i
+(startingYear..endingYear).each do |year|
+  puts "#{year} is Leap year" if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 end
